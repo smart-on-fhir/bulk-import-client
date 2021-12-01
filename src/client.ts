4d130af2a8b834ff3d3b64780e1487f41f799ebd
Vladimir Ignatov <vlad.ignatov@gmail.com>
2021-12-01 10:36:40 -0500

    Move authorization to the client

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,8 @@
 import { EventEmitter }                from "events"
 import { Parameters }                  from "fhir/r4"
 import { CancelableRequest, Response } from "got/dist/source"
+import jose                            from "node-jose"
+import jwt                             from "jsonwebtoken"
 import got                             from "./got"
 import config                          from "./config"
 import { ImportClient }                from "../index"
@@ -238,11 +240,48 @@ export class Client extends EventEmitter
             })
         }
     }
+
+    async authorize(): Promise<string>
+    {
+        const { clientId, consumerTokenUrl, accessTokenLifetime, privateKey } = config;
+
+        const claims = {
+            iss: clientId,
+            sub: clientId,
+            aud: consumerTokenUrl,
+            exp: Math.round(Date.now() / 1000) + accessTokenLifetime,
+            jti: jose.util.randomBytes(10).toString("hex")
+        };
+
+        const key = await jose.JWK.asKey(privateKey, "json");
+
+        const token = jwt.sign(claims, key.toPEM(true), { algorithm: key.alg as jwt.Algorithm });
+
+        this.authRequest = got<ImportClient.TokenResponse>(consumerTokenUrl, {
+            method: "POST",
+            responseType: "json",
+            form: {
+                scope: "system/*.*",
+                grant_type: "client_credentials",
+                client_assertion_type: "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+                client_assertion: token
+            },
+            context: {
+                verbose: this.verbose
+            }
+        });
+
+        return this.authRequest.then(res => {
+            this.authRequest = null
+            return res.body.access_token
+        });
+    }
+
     public async kickOff(params: ImportClient.BulkDataImportParams)
     {
         if (!this.accessToken) {
             try {
-                this.accessToken = await authorize(this.verbose);
+                this.accessToken = await this.authorize();
             } catch (e) {
                 return this.onError(e)
             }
